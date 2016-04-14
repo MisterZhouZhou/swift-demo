2aabba9b464a722509b4125ece2b0a4a3fb13664
小溪彼岸 <m15038960016@163.com>
2016-04-14 18:18:14 +0800

    添加了CAShapeLayer&UIBezierPath动画效果

--- swift-demo/swift-demo/classes/details/动画效果/ShapeAndBezierPathViewController.swift
+++ swift-demo/swift-demo/classes/details/动画效果/ShapeAndBezierPathViewController.swift
@@ -1,26 +1,26 @@
 //
-//  AnimationListViewController.swift
+//  ShapeAndBezierPathViewController.swift
 //  swift-demo
 //
-//  Created by rayootech on 16/4/12.
+//  Created by rayootech on 16/4/14.
 //  Copyright © 2016年 rayootech. All rights reserved.
 //
 
 import UIKit
 
-class AnimationListViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
-   var listArray :NSArray = NSArray()
+class ShapeAndBezierPathViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource {
+    var dataArray :NSArray = NSArray()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "动画列表"
-        
+
+       self.title = "CAShapeLayer&UIBezierPath操作"
         let tableView = UITableView(frame: UIScreen.mainScreen().applicationFrame)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.tableFooterView = UIView()
         self.view.addSubview(tableView)
         
-        listArray = ["画基本图形","CAShapeLayer&UIBezierPath"]
+        dataArray = ["CAShaperLayer画矩形","UIBezierPath画矩形","CAShaperLayer画圆","UIBezierPath画圆","画曲线","画波浪曲线"]
     }
 
     /**
@@ -30,7 +30,7 @@ class AnimationListViewController: UIViewController ,UITableViewDelegate,UITable
         return 1
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return listArray.count
+        return dataArray.count
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let ID = "cell"
@@ -39,24 +39,47 @@ class AnimationListViewController: UIViewController ,UITableViewDelegate,UITable
             cell = UITableViewCell(style: .Default, reuseIdentifier: ID)
         }
         cell?.accessoryType = .DisclosureIndicator
-        cell?.textLabel?.text = "\(listArray[indexPath.row])"
+        cell?.textLabel?.text = "\(dataArray[indexPath.row])"
         cell?.selectionStyle = .None
         return cell!
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+          let vc = ShapeAndBezierPathDetailViewController()
         switch indexPath.row{
         case 0:
-            self.navigationController?.pushViewController(DrawCircleViewController(), animated: true)
+            vc.layerType = LayerType.LayerType_Shapelayer_Rect
+            self.navigationController?.pushViewController(vc, animated: true)
             break
         case 1:
-            self.navigationController?.pushViewController(ShapeAndBezierPathViewController(), animated: true)
+             vc.layerType = LayerType.LayerType_BezierPath_Rect
+            self.navigationController?.pushViewController(vc, animated: true)
             break
+        case 2:
+           
+            vc.layerType = LayerType.LayerType_Shapelayer_Circle
+            self.navigationController?.pushViewController(vc, animated: true)
+            break
+        case 3:
+            vc.layerType = LayerType.LayerType_BezierPath_Circle
+            self.navigationController?.pushViewController(vc, animated: true)
+            break
+
+        case 4:
+            vc.layerType = LayerType.LayerType_CirRound
+            self.navigationController?.pushViewController(vc, animated: true)
+            break
+        case 5:
+            vc.layerType = LayerType.LayerType_CirRound_Double
+            self.navigationController?.pushViewController(vc, animated: true)
+            break
+
         default:
             break
         }
         
     }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
